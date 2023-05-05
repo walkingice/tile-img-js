3100570e85cd840b79c78c2e8f313387f25b71a2
Julian_Chu <walkingice0204@gmail.com>
2023-05-05 18:49:59 +0900

    read meatadata

--- concept/test_read.js
+++ concept/test_read.js
@@ -1,3 +1,34 @@
-const png = require('pngjs').PNG
+const fs = require('fs')
+const pngjs = require('pngjs')
 
-console.log("Hi..., ", png)
+const pipeline = require('node:stream').promises.pipeline
+
+const srcDir = "concept/assets"
+
+const filesPath = [
+    `${srcDir}/img_1.png`,
+    `${srcDir}/img_2.png`,
+    `${srcDir}/img_3.png`,
+    `${srcDir}/img_4.png`,
+]
+
+function readMeatadata(fileName) {
+    let fsStream = fs.createReadStream(fileName)
+    let pngStream = new pngjs.PNG({ filterType: 4 })
+    return new Promise((resolve, reject) => {
+        fsStream.pipe(pngStream)
+            .on('metadata', (metadata) => {
+                resolve({ fileName, metadata })
+            })
+    })
+}
+
+async function read(relativeFilePath) {
+    const promises = relativeFilePath.map(path => readMeatadata(path))
+    const results = await Promise.all(promises)
+    results.forEach((result) => {
+        console.log(`${result.fileName}, ${JSON.stringify(result.metadata)}`)
+    });
+}
+
+read(filesPath)
