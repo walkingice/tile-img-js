b2e5d433eecf61cd8d740f311570c1484e7fd947
Julian_Chu <walkingice0204@gmail.com>
2023-06-06 01:28:56 +0900

    remove dummy test file

--- src/test/dummy/Dummy.ts
+++
@@ -1,3 +0,0 @@
-export class Dummy {
-    greeting: string = "Dummy class"
-}
--- src/test/main.test.ts
+++
@@ -1,8 +0,0 @@
-import { Dummy } from './dummy/Dummy'
-
-test('Verify dummy strings', () => {
-    const dummy: Dummy = new Dummy()
-    console.log(dummy.greeting)
-    // Assert
-    expect(dummy.greeting).toBe("Dummy class");
-});
